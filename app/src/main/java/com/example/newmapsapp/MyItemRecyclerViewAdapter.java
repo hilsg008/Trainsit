ce3226e423ad695a8660dba03879f1fb12d92097
hilsg008 <112515064+hilsg008@users.noreply.github.com>
2023-09-16 20:53:29 -0500

    Created a working BottomListAbleItem list

---
+++ app/src/main/java/com/example/newmapsapp/MyItemRecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.newmapsapp;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newmapsapp.databinding.FragmentItemBinding;
+import com.example.newmapsapp.placeholder.BottomListAble.BottomListAbleItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link BottomListAbleItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<BottomListAbleItem> mValues;
+
+    public MyItemRecyclerViewAdapter(List<BottomListAbleItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public BottomListAbleItem mItem;
+
+        public ViewHolder(FragmentItemBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.method;
+            mContentView = binding.time;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
