076a95f85e3b3d0e26c8f959d8ec31a9c267c70a
hilsg008 <112515064+hilsg008@users.noreply.github.com>
2023-09-18 12:14:09 -0500

    Replaced BottomListAble with BottomListAbleItem

--- app/src/main/java/com/example/newmapsapp/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/example/newmapsapp/MyItemRecyclerViewAdapter.java
@@ -7,19 +7,19 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.newmapsapp.databinding.FragmentItemBinding;
-import com.example.newmapsapp.placeholder.BottomListAbleItem;
+import com.example.newmapsapp.placeholder.BottomListAble;
 
 import java.util.List;
 
 /**
- * {@link RecyclerView.Adapter} that can display a {@link BottomListAbleItem}.
+ * {@link RecyclerView.Adapter} that can display a {@link BottomListAble}.
  * TODO: Replace the implementation with code for your data type.
  */
 public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
 
-    private final List<BottomListAbleItem> mValues;
+    private final List<BottomListAble> mValues;
 
-    public MyItemRecyclerViewAdapter(List<BottomListAbleItem> items) {
+    public MyItemRecyclerViewAdapter(List<BottomListAble> items) {
         mValues = items;
     }
 
@@ -33,7 +33,6 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
         holder.mContentView.setText(mValues.get(position).content);
     }
 
@@ -43,13 +42,11 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public final TextView mIdView;
         public final TextView mContentView;
-        public BottomListAbleItem mItem;
+        public BottomListAble mItem;
 
         public ViewHolder(FragmentItemBinding binding) {
             super(binding.getRoot());
-            mIdView = binding.method;
             mContentView = binding.time;
         }
 
--- app/src/main/java/com/example/newmapsapp/placeholder/BottomListAble.java
+++ app/src/main/java/com/example/newmapsapp/placeholder/BottomListAble.java
@@ -1,49 +1,20 @@
 package com.example.newmapsapp.placeholder;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class BottomListAble {
+    public final String content;
+    public final String details;
 
-    /**
-     * An array of sample (placeholder) items.
-     */
-    public static final List<BottomListAbleItem> ITEMS = new ArrayList<BottomListAbleItem>();
-
-    private static final int COUNT = 25;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createPlaceholderItem(i));
-        }
+    public BottomListAble(String content, String details) {
+        this.content = content;
+        this.details = details;
     }
 
-    private static void addItem(BottomListAbleItem item) {
-        ITEMS.add(item);
+    public BottomListAble() {
+        this("content", "details");
     }
 
-    private static BottomListAbleItem createPlaceholderItem(int position) {
-        return new BottomListAbleItem(String.valueOf(position), "Item " + position, makeDetails(position));
+    @Override
+    public String toString() {
+        return content;
     }
-
-    private static String makeDetails(int position) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Details about Item: ").append(position);
-        for (int i = 0; i < position; i++) {
-            builder.append("\nMore details information here.");
-        }
-        return builder.toString();
-    }
-
-    /**
-     * A placeholder item representing a piece of content.
-     */
-
 }
