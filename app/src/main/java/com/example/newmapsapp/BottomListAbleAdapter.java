1ba6a80ffcc3fdde7f361d27724f569676b3476f
hilsg008 <112515064+hilsg008@users.noreply.github.com>
2023-09-23 21:10:55 -0500

    \getView in BottomListAbleAdapter first checks if the item has a view

--- app/src/main/java/com/example/newmapsapp/BottomListAbleAdapter.java
+++ app/src/main/java/com/example/newmapsapp/BottomListAbleAdapter.java
@@ -34,11 +34,16 @@ public class BottomListAbleAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        View convertView = layoutInflater.inflate(R.layout.bottom_listable_layout, null);
-        TextView test = (TextView) convertView.findViewById(R.id.method);
-        TextView test2 = (TextView) convertView.findViewById(R.id.time);
-        test.setText(items[i].getMethod());
-        test2.setText(items[i].getTime());
-        return convertView;
+        View defaultView = items[i].getView();
+        if(defaultView == null) {
+            View convertView = layoutInflater.inflate(R.layout.bottom_listable_layout, null);
+            TextView test = (TextView) convertView.findViewById(R.id.method);
+            TextView test2 = (TextView) convertView.findViewById(R.id.time);
+            test.setText(items[i].getMethod());
+            test2.setText(items[i].getTime());
+            return convertView;
+        } else {
+            return defaultView;
+        }
     }
 }
