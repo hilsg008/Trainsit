01fd93d99f0e3482e11d93aeadc01feba70c9c7f
hilsg008 <112515064+hilsg008@users.noreply.github.com>
2023-10-03 14:24:32 -0500

    Migrated HomeLayout to Fragment, can now use actions to switch between Fragments.

---
+++ app/src/main/java/com/example/newmapsapp/HomeLayoutFragment.java
@@ -0,0 +1,31 @@
+package com.example.newmapsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.newmapsapp.databinding.HomeLayoutBinding;
+
+public class HomeLayoutFragment extends Fragment {
+
+    private HomeLayoutBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = HomeLayoutBinding.inflate(inflater, container, false);
+        ListView listView = binding.bottomList;
+        BottomListAble[] b = new BottomListAble[]{new BottomListAble(), new Location(0,0), ExampleClasses.getRoutes()[0] ,new Path(new Route[][]{{new Route(ExampleClasses.getLocations())}})};
+        listView.setAdapter(new BottomListAbleAdapter(inflater.getContext(), b));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}
