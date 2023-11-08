62980ef2f7e2f6fe79dc95699aaafbdf907c92ae
hilsg008 <112515064+hilsg008@users.noreply.github.com>
2023-11-07 20:25:56 -0600

    Clicking toplist now leads to SearchLayoutFragment. FOR SOME REASON SEARCHLAYOUT HAS 2 TOPLISTS

--- app/src/main/java/com/example/newmapsapp/fragment/SearchLayoutFragment.java
+++ app/src/main/java/com/example/newmapsapp/fragment/SearchLayoutFragment.java
@@ -5,31 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.newmapsapp.R;
 import com.example.newmapsapp.adapter.BottomListAbleAdapter;
 import com.example.newmapsapp.bottomlistable.BottomListAble;
-import com.example.newmapsapp.bottomlistable.Route;
-import com.example.newmapsapp.databinding.RouteLayoutBinding;
 import com.example.newmapsapp.databinding.SearchLayoutBinding;
-import com.example.newmapsapp.viewmodel.RouteViewModel;
 
 public class SearchLayoutFragment extends Fragment {
 
     private SearchLayoutBinding binding;
     private MapsFragment map;
+    private TopListFragment topList;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         map = new MapsFragment();
+        topList = new TopListFragment();
         getChildFragmentManager()
                 .beginTransaction()
                 .add(R.id.map_container, map)
+                .add(R.id.top_list, topList)
                 .commit();
         binding = SearchLayoutBinding.inflate(inflater, container, false);
         ListView listView = binding.bottomList;
--- app/src/main/java/com/example/newmapsapp/fragment/TopListFragment.java
+++ app/src/main/java/com/example/newmapsapp/fragment/TopListFragment.java
@@ -1,16 +1,17 @@
 package com.example.newmapsapp.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.example.newmapsapp.ExampleClasses;
 import com.example.newmapsapp.R;
@@ -18,13 +19,40 @@ import com.example.newmapsapp.databinding.TopListBinding;
 
 public class TopListFragment extends Fragment {
     TopListBinding binding;
+    private boolean isSearchFragment = false;
+    private boolean startLocSelected = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("ThisIsATag", "onCreate");
         binding = TopListBinding.inflate(inflater, container, false);
-        binding.endLoc.setOnClickListener(ExampleClasses.getSearchListener());
-        binding.endLoc.setOnFocusChangeListener(ExampleClasses.getFocusListener());
-        binding.startLoc.setOnClickListener(ExampleClasses.getSearchListener());
-        binding.startLoc.setOnFocusChangeListener(ExampleClasses.getFocusListener());
+        setListeners();
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d("ThisIsATag", "viewCreated");
+        if(isSearchFragment) {
+            EditText v = startLocSelected ? view.findViewById(R.id.startLoc) : view.findViewById(R.id.endLoc);
+            if(v.requestFocus()) {
+                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT);
+            }
+        }
+    }
+
+    public void isSearchFragment(boolean isStartLoc) {
+        Log.d("ThisIsATag", "IsSearch");
+        isSearchFragment = true;
+        startLocSelected = isStartLoc;
+    }
+
+    private void setListeners() {
+        if(!isSearchFragment) {
+            binding.endLoc.setOnClickListener(ExampleClasses.getSearchListener());
+            binding.endLoc.setOnFocusChangeListener(ExampleClasses.getFocusListener());
+            binding.startLoc.setOnClickListener(ExampleClasses.getSearchListener());
+            binding.startLoc.setOnFocusChangeListener(ExampleClasses.getFocusListener());
+        }
     }
 }
